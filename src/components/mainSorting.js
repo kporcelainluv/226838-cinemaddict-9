c6b8bbcdff2cf7cf8e050748b7c9c5769b23b1d2
Ksusha Zhukova <zhukovaksusha@gmail.com>
2019-10-07 15:48:44 +0300

    add data sort type

--- src/components/mainSorting.js
+++ src/components/mainSorting.js
@@ -5,9 +5,10 @@ class MainSorting extends AbstractComponent {
     super();
   }
   getTemplate() {
-    return `<ul class="sort"><li><a href="#" class="sort__button sort__button--active">Sort by default</a></li>
-    <li><a href="#" class="sort__button">Sort by date</a></li>
-    <li><a href="#" class="sort__button">Sort by rating</a></li>
+    return `<ul class="sort">
+    <li><a href="#" class="sort__button sort__button--active" data-sort-type="default">Sort by default</a></li>
+    <li><a href="#" class="sort__button" data-sort-type="date">Sort by date</a></li>
+    <li><a href="#" class="sort__button" data-sort-type="rating">Sort by rating</a></li>
     </ul>`;
   }
 }
