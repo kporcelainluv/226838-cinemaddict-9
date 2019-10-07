e2652cfa831b6d019847f178f9fe9645ba739014
Ksusha Zhukova <zhukovaksusha@gmail.com>
2019-10-07 17:00:00 +0300

    add page controller

--- src/controllers/page-controller.js
+++ src/controllers/page-controller.js
@@ -1,7 +1,7 @@
 import { Film } from "../components/film";
 import { Popup } from "../components/popup";
 import { render, unrender, Position } from "../components/utils";
-import { SortingController } from "../controllers/sort-controller";
+import { MainSorting } from "../components/mainSorting";
 
 class PageController {
   constructor(container, mainPageContainer, data) {
@@ -12,18 +12,21 @@ class PageController {
       ".films-list__container"
     );
     this._body = document.getElementsByTagName("body")[0];
-    this._sortingController = new SortingController(
-      this._mainPageContainer,
-      this._data,
-      this._renderFilm.bind(this)
-    );
+    this._sort = new MainSorting();
   }
   init() {
     render(this._container, this._filmsListContainer, Position.AFTERBEGIN);
     for (let film of this._data) {
       this._renderFilm(film);
     }
-    this._sortingController.init();
+    render(
+      this._mainPageContainer,
+      this._sort.getElement(),
+      Position.AFTERBEGIN
+    );
+    this._sort
+      .getElement()
+      .addEventListener(`click`, evt => this._onSortLinkClick(evt));
   }
 
   _renderFilm(film) {
@@ -48,6 +51,40 @@ class PageController {
       unrender(popUpTemplate);
     });
     render(this._filmsListContainer, filmCard, Position.AFTERBEGIN);
+  }
+
+  _sortedByDateFilms(films) {
+    return films.sort((a, b) => {
+      return parseInt(a.year, 10) - parseInt(b.year, 10);
+    });
+  }
+  _sortedByRatingFilms(films) {
+    return films.sort((a, b) => {
+      return parseInt(a.rating, 10) - parseInt(b.rating, 10);
+    });
+  }
+  _onSortLinkClick(evt) {
+    evt.preventDefault();
+
+    if (evt.target.tagName !== `A`) {
+      return;
+    }
+    this._filmsListContainer.innerHTML = "";
+
+    switch (evt.target.dataset.sortType) {
+      case `default`:
+        const sortedByDefault = this._data;
+        sortedByDefault.forEach(mock => this._renderFilm(mock));
+        break;
+      case `date`:
+        const sortedByDate = this._sortedByDateFilms(this._data);
+        sortedByDate.forEach(mock => this._renderFilm(mock));
+        break;
+      case `rating`:
+        const sortedByRating = this._sortedByRatingFilms(this._data);
+        sortedByRating.forEach(mock => this._renderFilm(mock));
+        break;
+    }
   }
 }
 export { PageController };
