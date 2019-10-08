ee22d9bb1d8f6ad72fa75c8fd6c4518bd1e9cd54
Ksusha Zhukova <zhukovaksusha@gmail.com>
2019-10-08 20:06:52 +0300

    add onDataChange

--- src/controllers/movie-controller.js
+++ src/controllers/movie-controller.js
@@ -3,15 +3,19 @@ import { Position, render, unrender } from "../components/utils";
 import { Film } from "../components/film";
 
 class MovieController {
-  constructor(container, films, onDataChange, onChangeView) {
+  constructor(container, film, onDataChange, onChangeView) {
+    this._filmCard = new Film(film).getElement();
+    this._popUpTemplate = new Popup(film).getElement();
+
     this._container = container;
-    this._film = films;
+    this._film = film;
     this._onDataChange = onDataChange;
     this._onChangeView = onChangeView;
 
-    this._filmCard = new Film(this._film).getElement();
-    this._popUpTemplate = new Popup(this._film).getElement();
     this._commentsButton = this._filmCard.querySelector(`.film-card__comments`);
+    this._popUPCloseButton = this._popUpTemplate.querySelector(
+      `.film-details__close-btn`
+    );
     this._body = document.getElementsByTagName("body")[0];
   }
 
@@ -27,7 +31,46 @@ class MovieController {
       render(this._body, this._popUpTemplate, "beforeend");
       document.addEventListener(`keydown`, onEscKeyDown);
     });
+
+    this._popUPCloseButton.addEventListener("click", () => {
+      unrender(this._popUpTemplate);
+    });
+
     render(this._container, this._filmCard, Position.AFTERBEGIN);
+
+    // check if in favorites, in watchlist, in watched
+    this._filmCard
+      .querySelector(".film-card__controls-item--add-to-watchlist")
+      .addEventListener("click", evt => {
+        evt.preventDefault();
+        const updatedFilm = {
+          ...this._film,
+          isWatchlist: !this._film.isWatchlist
+        };
+        this._onDataChange(updatedFilm);
+      });
+
+    this._filmCard
+      .querySelector(".film-card__controls-item--mark-as-watched")
+      .addEventListener("click", evt => {
+        evt.preventDefault();
+        const updatedFilm = {
+          ...this._film,
+          isWatched: !this._film.isWatched
+        };
+        this._onDataChange(updatedFilm);
+      });
+
+    this._filmCard
+      .querySelector(".film-card__controls-item--favorite")
+      .addEventListener("click", evt => {
+        evt.preventDefault();
+        const updatedFilm = {
+          ...this._film,
+          isFavorite: !this._film.isFavorite
+        };
+        this._onDataChange(updatedFilm);
+      });
   }
 }
 export { MovieController };
