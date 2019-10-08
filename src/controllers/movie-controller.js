116c333ee91a10d6caf6eb112310a28209f11d78
Ksusha Zhukova <zhukovaksusha@gmail.com>
2019-10-08 23:39:16 +0300

    add popup event listeners

--- src/controllers/movie-controller.js
+++ src/controllers/movie-controller.js
@@ -17,6 +17,9 @@ class MovieController {
       `.film-details__close-btn`
     );
     this._body = document.getElementsByTagName("body")[0];
+    this._emojiLabel = this._popUpTemplate.querySelectorAll(
+      `.film-details__emoji-item`
+    );
   }
 
   init() {
@@ -71,6 +74,49 @@ class MovieController {
         };
         this._onDataChange(updatedFilm);
       });
+    // popUp event listeners
+
+    this._popUpTemplate
+      .querySelector(".film-details__control-label--watched")
+      .addEventListener("click", evt => {
+        evt.preventDefault();
+        console.log("watched");
+        const updatedFilm = {
+          ...this._film,
+          isWatched: !this._film.isWatched
+        };
+        this._onDataChange(updatedFilm);
+      });
+    this._popUpTemplate
+      .querySelector(".film-details__control-label--watchlist")
+      .addEventListener("click", evt => {
+        evt.preventDefault();
+        console.log("watchlist");
+        const updatedFilm = {
+          ...this._film,
+          isWatchlist: !this._film.isWatchlist
+        };
+        this._onDataChange(updatedFilm);
+      });
+    this._popUpTemplate
+      .querySelector(".film-details__control-label--favorite")
+      .addEventListener("click", evt => {
+        evt.preventDefault();
+        console.log("fav");
+        this._film = {
+          ...this._film,
+          isFavorite: !this._film.isFavorite
+        };
+        this._onDataChange(this._film);
+
+      });
+    // adding emoji
+    for (let emoji of this._emojiLabel) {
+      emoji.addEventListener("click", evt => {
+        evt.preventDefault();
+        console.log(evt.target.id);
+      });
+    }
   }
 }
 export { MovieController };
