cba4ec9ddb8dede7c692d95e8f2ee024b2fccb03
Ksusha Zhukova <zhukovaksusha@gmail.com>
2019-10-09 18:19:03 +0300

    task 3

--- src/controllers/1.js
+++ src/controllers/1.js
@@ -427,6 +427,93 @@ class TaskController {
     this._data = data;
     this._taskView = new Task(data);
     this._taskEdit = new TaskEdit(data);
+
+    this.create();
+  }
+
+  create() {
+    const onEscKeyDown = evt => {
+      if (evt.key === Key.ESCAPE || evt.key === Key.ESCAPE_IE) {
+        this._container
+          .getElement()
+          .replaceChild(
+            this._taskView.getElement(),
+            this._taskEdit.getElement()
+          );
+        document.removeEventListener(`keydown`, onEscKeyDown);
+      }
+    };
+
+    this._taskEdit
+      .getElement()
+      .querySelector(`textarea`)
+      .addEventListener(`focus`, () => {
+        document.removeEventListener(`keydown`, onEscKeyDown);
+      });
+
+    this._taskEdit
+      .getElement()
+      .querySelector(`textarea`)
+      .addEventListener(`blur`, () => {
+        document.addEventListener(`keydown`, onEscKeyDown);
+      });
+
+    this._taskView
+      .getElement()
+      .querySelector(`.card__btn--edit`)
+      .addEventListener(`click`, evt => {
+        evt.preventDefault();
+        this._onChangeView();
+        this._container
+          .getElement()
+          .replaceChild(
+            this._taskEdit.getElement(),
+            this._taskView.getElement()
+          );
+
+        document.addEventListener(`keydown`, onEscKeyDown);
+      });
+
+    this._taskEdit
+      .getElement()
+      .querySelector(`.card__save`)
+      .addEventListener(`click`, evt => {
+        evt.preventDefault();
+
+        const formData = new FormData(
+          this._taskEdit.getElement().querySelector(`.card__form`)
+        );
+
+        const entry = {
+          description: formData.get(`text`),
+          color: formData.get(`color`),
+          tags: new Set(formData.getAll(`hashtag`)),
+          dueDate: new Date(formData.get(`date`)),
+          repeatingDays: formData.getAll(`repeat`).reduce(
+            (acc, it) => {
+              acc[it] = true;
+              return acc;
+            },
+            {
+              mo: false,
+              tu: false,
+              we: false,
+              th: false,
+              fr: false,
+              sa: false,
+              su: false
+            }
+          )
+        };
+
+        document.removeEventListener(`keydown`, onEscKeyDown);
+      });
+
+    render(
+      this._container.getElement(),
+      this._taskView.getElement(),
+      Position.BEFOREEND
+    );
   }
 }
 
