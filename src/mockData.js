546d56a86f3f7230dd4d17d416fd7f30845ce6c2
Ksusha Zhukova <zhukovaksusha@gmail.com>
2019-10-17 20:42:03 +0300

    Move mock data

---
+++ src/mockData.js
@@ -0,0 +1,140 @@
+export const mockFilms = [
+  {
+    id: 0,
+    name: `The Great Gatsby`,
+    rating: `10`,
+    genre: `drama`,
+    year: `2013`,
+    time: `1h 16m`,
+    // poster: "made-for-each-other",
+    descriptionText: `Hello world!`,
+    ageRating: `18+`,
+    titleOriginal: `The Great Gatsby by S.Fitzgerald`,
+    details: `Hello`,
+    personalRating: `10`,
+    director: `ksusha`,
+    writers: `yanis`,
+    actors: `polina`,
+    date: `04.05.1996`,
+    runtime: `2h`,
+    country: `usa`,
+    isWatchlist: false,
+    isWatched: false,
+    isFavorite: false,
+    comments: [
+      {
+        id: 0,
+        text: "Absurd",
+        name: "Alison Cruze",
+        date: "19/10/2019 17:20",
+        emoji: "puke"
+      },
+      {
+        id: 1,
+        text: "Boring",
+        name: "Michelle Obama",
+        date: "19/12/2018 12:10",
+        emoji: "angry"
+      },
+      {
+        id: 2,
+        text: "Amusing",
+        name: "Sarrah Fergusson",
+        date: "29/02/2019 01:12",
+        emoji: "sleeping"
+      }
+    ]
+  },
+  {
+    id: 1,
+    name: `Inception`,
+    rating: `8`,
+    genre: `action`,
+    year: `2011`,
+    time: `3h 16m`,
+    // poster: "popeye-meets-sinbad",
+    descriptionText: `OMG!!!`,
+    ageRating: `4+`,
+    titleOriginal: `Inception into the brain`,
+    details: `Wonderful`,
+    personalRating: `8`,
+    director: `ksusha`,
+    writers: `yanis`,
+    actors: `polina`,
+    date: `04.05.1996`,
+    runtime: `2h`,
+    country: `usa`,
+    isWatchlist: false,
+    isWatched: false,
+    isFavorite: false,
+    comments: [
+      {
+        id: 0,
+        text: "Absurd",
+        name: "Alison Cruze",
+        date: "19/10/2019 17:20",
+        emoji: "puke"
+      },
+      {
+        id: 1,
+        text: "Boring",
+        name: "Michelle Obama",
+        date: "19/12/2018 12:10",
+        emoji: "angry"
+      },
+      {
+        id: 2,
+        text: "Amusing",
+        name: "Sarrah Fergusson",
+        date: "29/02/2019 01:12",
+        emoji: "sleeping"
+      }
+    ]
+  },
+  {
+    id: 2,
+    name: `Joker`,
+    genre: `thriller`,
+    year: `2019`,
+    time: `2h 10m`,
+    rating: `7`,
+    // poster: "popeye-meets-sinbad",
+    descriptionText: `YES!!!`,
+    ageRating: `18+`,
+    titleOriginal: `Crazy`,
+    details: `Scary`,
+    personalRating: `7`,
+    director: `ksusha`,
+    writers: `yanis`,
+    actors: `polina`,
+    date: `04.05.1996`,
+    runtime: `2h`,
+    country: `usa`,
+    isWatchlist: false,
+    isWatched: false,
+    isFavorite: false,
+    comments: [
+      {
+        id: 0,
+        text: "Absurd",
+        name: "Alison Cruze",
+        date: "19/10/2019 17:20",
+        emoji: "puke"
+      },
+      {
+        id: 1,
+        text: "Boring",
+        name: "Michelle Obama",
+        date: "19/12/2018 12:10",
+        emoji: "angry"
+      },
+      {
+        id: 2,
+        text: "Amusing",
+        name: "Sarrah Fergusson",
+        date: "29/02/2019 01:12",
+        emoji: "sleeping"
+      }
+    ]
+  }
+];
